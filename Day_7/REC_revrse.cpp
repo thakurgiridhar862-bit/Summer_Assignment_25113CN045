4c817c9c6a8dd80af9d62235885b89b27b428b0a
thakurgiridhar862-bit <thakurgiridhar862@gmail.com>
2026-06-06 08:26:43 +0530

    Day 7 uploaded

---
+++ Day_7/REC_revrse.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int rev = 0;
+
+void reverseNum(int n)
+{
+    if (n == 0)
+        return;
+
+    rev = rev * 10 + (n % 10);
+    reverseNum(n / 10);
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    reverseNum(n);
+    cout << rev;
+
+    return 0;
+}
